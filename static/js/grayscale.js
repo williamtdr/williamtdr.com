435f9baff01e94e942dc5be3aa4084f1c14fc7ca
William Teder <wteder@hydreon.com>
2016-06-06 13:12:16 -0500

    Add bio content

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -17,12 +17,12 @@ $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {
-	$("a.page-scroll").bind("click", function (event) {
+$(function() {
+	$("a.page-scroll").bind("click", function(event) {
 		var $anchor = $(this);
 
 		$("html, body").stop().animate({
-			scrollTop: $($anchor.attr("href")).offset().top
+			scrollTop: $($anchor.attr("href")).offset().top - 50
 		}, 1500, "easeInOutExpo");
 		event.preventDefault();
 	});
