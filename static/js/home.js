5277650a129e700f68d9df125d86d39521c44af5
William Teder <wteder@hydreon.com>
2019-08-29 02:10:27 -0500

    fix(home.links): email onclick trigger

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,4 @@
-$("#link-email").click(function(ev) {
+$(".link-email").click(function(ev) {
 	ev.preventDefault();
 	window.location.href = atob("bWFpbHRvOg==") + atob("d3RlZGVy") + "@" + atob("aHlkcmVvbi5jb20=");
 });
